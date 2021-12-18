37d360ab3a95b685f4ff58f372da854d8ffdef73
jkim68888 <jkim68888@gmail.com>
2021-12-18 16:53:59 +0900

    홈 인기카테고리 퍼블리싱 완료

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,13 +1,50 @@
 import * as React from 'react'
-import { HomeContainer } from '../../styles/Home.styles'
+import {
+  HomeContainer,
+  CategoryBanner,
+  BlueBanner,
+  PopularPostBanner,
+  LatestPostBanner,
+} from '../../styles/Home.styles'
 import Search from '../../components/Search'
 
 interface IHomeProps {}
+
+const categories = [
+  { id: '0', imgUrl: '/assets/img/vaccine.svg', name: '백신' },
+  { id: '1', imgUrl: '/assets/img/cold.svg', name: '감기' },
+  { id: '2', imgUrl: '/assets/img/headache.svg', name: '두통' },
+  { id: '3', imgUrl: '/assets/img/tooth.svg', name: '치통' },
+  { id: '4', imgUrl: '/assets/img/bug.svg', name: '벌레물림' },
+  { id: '5', imgUrl: '/assets/img/muscle.svg', name: '근육통' },
+  { id: '6', imgUrl: '/assets/img/virus.svg', name: '바이러스' },
+  { id: '7', imgUrl: '/assets/img/stomache.svg', name: '복통' },
+]
 
 export default function Home(props: IHomeProps) {
   return (
     <HomeContainer>
       <Search />
+      <CategoryBanner className="wrap">
+        <div className="title">인기 카테고리</div>
+        <div className="categoryItemContainer">
+          {categories.map((item) => {
+            return (
+              <div className="categoryItem" key={`category: ${item.id}`}>
+                <img src={item.imgUrl} alt={`${item.name} 아이콘`} />
+                <h2>{item.name}</h2>
+              </div>
+            )
+          })}
+        </div>
+      </CategoryBanner>
+      <BlueBanner></BlueBanner>
+      <PopularPostBanner className="wrap">
+        <div className="title">인기 게시글</div>
+      </PopularPostBanner>
+      <LatestPostBanner className="wrap">
+        <div className="title">최신 게시글</div>
+      </LatestPostBanner>
     </HomeContainer>
   )
 }
--- frontend/src/styles/Home.styles.ts
+++ frontend/src/styles/Home.styles.ts
@@ -3,3 +3,41 @@ import styled from 'styled-components'
 export const HomeContainer = styled.div`
   padding-top: 170px;
 `
+export const CategoryBanner = styled.div`
+  padding: 70px 0 125px;
+
+  .categoryItemContainer {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 150px 14.8%;
+
+    img {
+      transition: transform 0.8s;
+      transform-style: preserve-3d;
+    }
+
+    .categoryItem {
+      cursor: pointer;
+
+      h2 {
+        margin-top: 18px;
+        text-align: center;
+        font-size: 30px;
+        font-weight: normal;
+      }
+
+      &:hover img {
+        transform: rotateY(180deg);
+      }
+    }
+  }
+`
+export const BlueBanner = styled.div`
+  padding: 70px 0 125px;
+`
+export const PopularPostBanner = styled.div`
+  padding: 70px 0 125px;
+`
+export const LatestPostBanner = styled.div`
+  padding: 70px 0 125px;
+`

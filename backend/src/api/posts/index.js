9a5cfc36a0d2f51bcea3a26209c715fd69632c30
SeongBeom27 <lgh121546@naver.com>
2021-12-19 13:36:05 +0900

    [API] Post List 불러오기 구현

--- backend/src/api/posts/index.js
+++ backend/src/api/posts/index.js
@@ -1,8 +1,10 @@
 const Router = require('koa-router');
 import * as postsCtrl from './posts.ctrl';
+import checkLoggedIn from '../../lib/checkLoggedIn';
 
 const posts = new Router();
 
-posts.post('/', postsCtrl.write);
+posts.post('/write', postsCtrl.write);
+posts.get('/list', postsCtrl.list);
 
 export default posts;
--- backend/src/api/posts/posts.ctrl.js
+++ backend/src/api/posts/posts.ctrl.js
@@ -1,23 +1,50 @@
 import Post from '../../models/post';
 import mongoose from 'mongoose';
+import sanitizeHtml from 'sanitize-html';
 import Joi from 'joi';
 
 const { ObjectId } = mongoose.Types;
 
-/**
- * 특정 포스트 조회
- * GET /api/posts/:id
- */
-export const read = async (ctx) => {
-  ctx.body = 'read!';
+const removeHtmlAndShorten = (body) => {
+  const filtered = sanitizeHtml(body, {
+    allowedTags: [],
+  });
+  return filtered.length < 200 ? filtered : `${filtered.slice(0, 200)}...`;
+};
+
+/*
+  GET /api/posts?nick=&category=
+*/
+export const list = async (ctx) => {
+  const { category, nick } = ctx.query;
+  // tag, nick 값이 유효하면 객체 안에 넣고, 그렇지 않으면 넣지 않음
+  const query = {
+    ...(nick ? { 'user.nick': nick } : {}),
+    ...(category ? { category: category } : {}),
+  };
+
+  try {
+    const posts = await Post.find(query)
+      .sort({ _id: -1 })
+      .limit(10)
+      .lean()
+      .exec();
+    ctx.body = posts.map((post) => ({
+      ...post,
+      body: removeHtmlAndShorten(post.body),
+    }));
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
 /**
  * 포스트 작성
- * POST /api/posts
+ * POST /api/posts/write
  * {
  *    title: '제목',
  *    body:  '내용',
+ *    tags: ['태그1', '태그2']
  * }
  */
 export const write = async (ctx) => {
@@ -25,8 +52,8 @@ export const write = async (ctx) => {
     // 객체가 다음 필드를 가지고 있음을 검증
     title: Joi.string().required(), // required가 있으면 필수항목
     body: Joi.string().required(),
+    category: Joi.string().required(),
   });
-
   const result = schema.validate(ctx.request.body);
   if (result.error) {
     ctx.status = 400; // Bad Request
@@ -34,10 +61,16 @@ export const write = async (ctx) => {
     return;
   }
   // REST API의 Reuqest Body는 ctx.request.body에서 조회 가능
-  const { title, body } = ctx.request.body;
+  const { title, body, category } = ctx.request.body;
   const post = new Post({
     title,
     body,
+    category,
+    views: 0,
+    // user: {
+    //   _id: ctx.req.user._id,
+    //   nick: ctx.req.user.nick,
+    // },
   });
   try {
     // async/await 문법으로 데이터베이스 저장 요청을 완료할 때 까지 대기

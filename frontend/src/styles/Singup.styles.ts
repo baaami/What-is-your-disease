0c52613b63909ae0b9375bec6e1fa95987185f29
jkim68888 <jkim68888@gmail.com>
2021-12-22 12:57:11 +0900

    로그인 페이지 퍼블리싱

---
+++ frontend/src/styles/Singup.styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const SignupWrapper = styled.div`
+  padding-top: 170px;
+  text-align: center;
+
+  h2 {
+    margin: 100px 0 50px;
+    font-weight: 400;
+    font-size: 24px;
+  }
+  div {
+    margin-bottom: 200px;
+  }
+  img {
+    margin: 0 15px;
+    border-radius: 100%;
+  }
+`

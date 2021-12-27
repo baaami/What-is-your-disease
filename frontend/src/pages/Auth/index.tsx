7b9d7ee7c39f0a3113734748bdbdb9f804b1a122
SeongBeom27 <lgh121546@naver.com>
2021-12-27 18:20:54 +0900

    [FrontEnd] /api/auth/callback/kakao 2회 요청 버그 수정

--- frontend/src/pages/Auth/index.tsx
+++ frontend/src/pages/Auth/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { useHistory } from 'react-router-dom'
+import { useEffect } from 'react'
 import client from '../../lib/client'
 import { PopularPostBanner } from '../../styles/Home.styles'
 
@@ -8,26 +9,25 @@ interface AuthProps {}
 export default function Auth(props: AuthProps) {
   const history = useHistory()
 
-  console.log('get url')
-  // 인가 코드
-  const code = new URL(window.location.href).searchParams.get('code')
+  useEffect(() => {
+    // 인가 코드
+    const code = new URL(window.location.href).searchParams.get('code')
 
-  console.log(code)
-
-  client
-    .post('/api/auth/callback/kakao', { code })
-    .then(res => {
-      console.log('jwt token : ', res.data) //예시로
-      // 예시로 로컬에 저장
-      localStorage.setItem('jwt token : ', res.data) //예시로 로컬에 저장함
-      window.alert('로그인에 성공하였습니다.')
-      history.replace('/') // 토큰 받았았고 로그인됐으니 화면 전환시켜줌(메인으로)
-    })
-    .catch(err => {
-      console.log('소셜로그인 에러', err)
-      window.alert('로그인에 실패하였습니다.')
-      history.replace('/login') // 로그인 실패하면 로그인화면으로 돌려보냄
-    })
+    client
+      .post('/api/auth/callback/kakao', { code })
+      .then(res => {
+        console.log('jwt token : ', res.data) //예시로
+        // 예시로 로컬에 저장
+        localStorage.setItem('jwt token : ', res.data) //예시로 로컬에 저장함
+        window.alert('로그인에 성공하였습니다.')
+        history.replace('/') // 토큰 받았았고 로그인됐으니 화면 전환시켜줌(메인으로)
+      })
+      .catch(err => {
+        console.log('소셜로그인 에러', err)
+        window.alert('로그인에 실패하였습니다.')
+        history.replace('/login') // 로그인 실패하면 로그인화면으로 돌려보냄
+      })
+  })
 
   return <div></div>
 }

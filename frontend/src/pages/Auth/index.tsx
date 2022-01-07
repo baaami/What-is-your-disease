22d5a6b1c0f8ebf3ebdd6bd193cc5dd3ccb2a65a
jkim68888 <jkim68888@gmail.com>
2022-01-07 14:30:34 +0900

    로그인 경로명 수정

--- frontend/src/pages/Auth/index.tsx
+++ frontend/src/pages/Auth/index.tsx
@@ -14,14 +14,14 @@ export default function Auth(props: AuthProps) {
 
     client
       .post('/api/auth/callback/kakao', { code })
-      .then(res => {
+      .then((res) => {
         console.log('Response Data : ', res.data) //예시로
         // 예시로 로컬에 저장
         localStorage.setItem('jwttoken', res.data.token) //예시로 로컬에 저장함
         window.alert('로그인에 성공하였습니다.')
         history.replace('/') // 토큰 받았았고 로그인됐으니 화면 전환시켜줌(메인으로)
       })
-      .catch(err => {
+      .catch((err) => {
         console.log('소셜로그인 에러', err)
         window.alert('로그인에 실패하였습니다.')
         history.replace('/login') // 로그인 실패하면 로그인화면으로 돌려보냄

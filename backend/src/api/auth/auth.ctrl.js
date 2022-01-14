c6c0ad4a3c97a974d19839d86f7f9a3b91b5621c
SeongBeom27 <lgh121546@naver.com>
2022-01-14 23:42:22 +0900

    [BackEnd] 네이버/구글 로그인 추가작업 중

--- backend/src/api/auth/auth.ctrl.js
+++ backend/src/api/auth/auth.ctrl.js
@@ -66,7 +66,156 @@ export const kakao = async (ctx) => {
     user = { ...CheckedUser };
   }
 
-  console.log('user : ', user);
+  // 자체 토큰 발급
+  const jwtToken = await jwt.sign(user);
+
+  result = {
+    token: jwtToken.token,
+    user: user,
+  };
+
+  // access token을 JWT를 사용하여 서버만의 토큰으로 발급 후 front에 전달
+  ctx.body = result;
+};
+
+export const naver = async (ctx) => {
+  // 인가 코드 획득
+  const { code } = ctx.request.body;
+  let RepToken, RepUser, result;
+
+  // access 토큰 요청
+  try {
+    RepToken = await axios({
+      method: 'POST',
+      url: 'https://kauth.kakao.com/oauth/token',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      data: qs.stringify({
+        grant_type: 'authorization_code',
+        client_id: process.env.CLIENT_ID,
+        redirect_uri: process.env.REDIRECT_URI,
+        code: code,
+      }),
+    });
+  } catch (err) {
+    console.log(err.response.data);
+  }
+
+  const { access_token } = RepToken.data;
+
+  // 유저 데이터 요청
+  try {
+    RepUser = await axios({
+      method: 'get',
+      url: 'https://kapi.kakao.com/v2/user/me',
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+  } catch (err) {
+    console.log(err.response.data);
+  }
+
+  let user;
+  const _id = mongoose.Types.ObjectId();
+  const { id } = RepUser.data;
+  const CheckedUser = await User.findByproviderId(id);
+
+  console.log('Checked User : ', CheckedUser);
+  if (!CheckedUser) {
+    // 첫번째 로그인
+    // 1. 카카오에서 전달 받은 User data에서 필요한 정보를 파싱하여 db에 저장
+    const NewUser = new User({
+      _id,
+      providerId: id,
+      provider: 'kakao',
+    });
+    await NewUser.save(); // 데이터베이스에 저장
+
+    user = await User.findById(_id);
+    // console.log('[TEST] First User : ', user);
+  } else {
+    // 두번째 로그인일 경우
+    user = { ...CheckedUser };
+    // console.log('[TEST] Exist User  : ', user);
+  }
+
+  // 자체 토큰 발급
+  const jwtToken = await jwt.sign(user);
+
+  result = {
+    token: jwtToken.token,
+    user: user,
+  };
+
+  // access token을 JWT를 사용하여 서버만의 토큰으로 발급 후 front에 전달
+  ctx.body = result;
+};
+
+export const google = async (ctx) => {
+  // 인가 코드 획득
+  const { code } = ctx.request.body;
+  let RepToken, RepUser, result;
+
+  // access 토큰 요청
+  try {
+    RepToken = await axios({
+      method: 'POST',
+      url: 'https://kauth.kakao.com/oauth/token',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      data: qs.stringify({
+        grant_type: 'authorization_code',
+        client_id: process.env.CLIENT_ID,
+        redirect_uri: process.env.REDIRECT_URI,
+        code: code,
+      }),
+    });
+  } catch (err) {
+    console.log(err.response.data);
+  }
+
+  const { access_token } = RepToken.data;
+
+  // 유저 데이터 요청
+  try {
+    RepUser = await axios({
+      method: 'get',
+      url: 'https://kapi.kakao.com/v2/user/me',
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+  } catch (err) {
+    console.log(err.response.data);
+  }
+
+  let user;
+  const _id = mongoose.Types.ObjectId();
+  const { id } = RepUser.data;
+  const CheckedUser = await User.findByproviderId(id);
+
+  console.log('Checked User : ', CheckedUser);
+  if (!CheckedUser) {
+    // 첫번째 로그인
+    // 1. 카카오에서 전달 받은 User data에서 필요한 정보를 파싱하여 db에 저장
+    const NewUser = new User({
+      _id,
+      providerId: id,
+      provider: 'kakao',
+    });
+    await NewUser.save(); // 데이터베이스에 저장
+
+    user = await User.findById(_id);
+    // console.log('[TEST] First User : ', user);
+  } else {
+    // 두번째 로그인일 경우
+    user = { ...CheckedUser };
+    // console.log('[TEST] Exist User  : ', user);
+  }
+
   // 자체 토큰 발급
   const jwtToken = await jwt.sign(user);
 
--- backend/src/api/auth/index.js
+++ backend/src/api/auth/index.js
@@ -5,6 +5,8 @@ import checkLoggedIn from '../../lib/checkLoggedIn';
 const auth = new Router();
 
 auth.post('/callback/kakao', authCtrl.kakao);
+auth.post('/callback/naver', authCtrl.naver);
+auth.post('/callback/google', authCtrl.google);
 auth.get('/logout', authCtrl.logout);
 
 export default auth;

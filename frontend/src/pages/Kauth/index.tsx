014135324d1c8fbc406d5fbeaadeef1065d85710
jeehoo0767 <jeehoo0767@naver.com>
2022-01-19 23:26:59 +0900

    로그인 응답값 변경에 따른 프론트 로직 수정

--- frontend/src/pages/Kauth/index.tsx
+++ frontend/src/pages/Kauth/index.tsx
@@ -18,30 +18,23 @@ export default function Kauth(props: KauthProps) {
     client
       .post('/api/auth/callback/kakao', { code })
       .then((res) => {
-        console.log('Response Data : ', res.data) //예시로
+        const { user, is_new, token } = res.data
         // 예시로 로컬에 저장
-        localStorage.setItem('jwttoken', res.data.token) //예시로 로컬에 저장함
-        const userInfo = res.data.user._doc
-        delete userInfo.__v
-        localStorage.setItem('userInfo', JSON.stringify(userInfo)) // 예시로 유저정보를 백엔드 유저 모델과 똑같이 로컬스토리지에 저장
+        localStorage.setItem('jwttoken', token) //예시로 로컬에 저장함
 
-        // 응답값에 필수 유저정보중에 하나라도 입력이 안되어 있다면
-        if (
-          res.data.user._doc.info.name === undefined ||
-          !res.data.user._doc.info.age === undefined ||
-          !res.data.user._doc.info.gender === undefined
-        ) {
+        // 첫번째 로그인 이라면
+        if (is_new) {
           // 회원정보 입력 페이지로 이동
           setUserInfo({
             ...userInfo,
-            ...res.data.user._doc,
+            ...user,
           })
           alert('로그인에 성공하였습니다. 회원 정보 입력 페이지로 이동합니다.')
           history.push('/infoForm')
         } else {
           setUserInfo({
             ...userInfo,
-            ...res.data.user._doc,
+            ...user,
           })
           history.replace('/') // 토큰 받았았고 로그인됐으니 화면 전환시켜줌(메인으로)
         }

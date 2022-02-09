77c5284fb0d53a4ba8eb5050c358e881659d2e9a
SeongBeom27 <lgh121546@naver.com>
2022-02-09 15:59:02 +0900

    [Backend] Post, Comment, Reply 스키마 변경 후 CRUD 변경

--- backend/src/api/post/comment/comment.ctrl.js
+++ backend/src/api/post/comment/comment.ctrl.js
@@ -1,43 +1,16 @@
 import mongoose from 'mongoose';
 import Post from '../../../models/post';
 import Comment from '../../../models/comment';
+
 import sanitizeHtml from 'sanitize-html';
 
 const { ObjectId } = mongoose.Types;
-/**
- * @brief Id를 통하여 Comment 검증 후 존재 시 ctx.state.comment로 전달
- *
- * @param {*} ctx
- * @param {*} next
- * @returns
- */
-export const checkCommentById = async (ctx, next) => {
-  const { commentId } = ctx.params;
-  if (!ObjectId.isValid(commentId)) {
-    ctx.status = 400;
-    return;
-  }
-
-  try {
-    const comment = await Comment.findById(commentId);
-
-    // 포스트가 존재하지 않을 때
-    if (!comment) {
-      ctx.status = 404;
-    }
-    ctx.state.comment = comment.toJSON();
-    return next();
-  } catch (e) {
-    ctx.throw(500, e);
-  }
-};
 
 /**
  * 댓글 등록
- * POST /api/post/comment/:postId
+ * POST /api/post/:postId/comment/write
  */
 export const cmUpload = async (ctx) => {
-  // 1. 필요한 값 할당
   const curPost = ctx.state.post;
   const { text } = ctx.request.body;
   const comment = new Comment({
@@ -47,25 +20,20 @@ export const cmUpload = async (ctx) => {
     user: ctx.state.user,
   });
 
+  // 해당 포스트 댓글 배열에 추가
   try {
-    const _ = await Post.findByIdAndUpdate(
+    const result = await Post.findByIdAndUpdate(
       curPost._id,
       {
-        $push: { commentIds: comment._id },
+        $push: { comments: comment },
       },
       {
         new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
         // false 일 때에는 업데이트 되기 전의 데이터를 반환합니다.
       },
     ).exec();
-  } catch (e) {
-    ctx.throw(500, e);
-  }
 
-  // 3. comment 생성
-  try {
-    await comment.save();
-    ctx.body = comment;
+    ctx.body = result;
   } catch (e) {
     ctx.throw(500, e);
   }
@@ -73,41 +41,31 @@ export const cmUpload = async (ctx) => {
 
 /**
  * 댓글 삭제
- * DELETE /api/post/comment/:postId/:commentId
+ * DELETE /api/post/:postId/comment/delete/:commentId
  */
 export const cmDelete = async (ctx) => {
   const curPost = ctx.state.post;
   const { commentId } = ctx.params;
-
-  // 해당 댓글 삭제 진행
-  // Type -> Id : new ObjectId
-  //         commenㅌtId : String
-  const newId = curPost.commentIds.filter((Id) => Id != commentId);
 
   // 1. post에서 해당 댓글 id 제거
   try {
     const post = await Post.findByIdAndUpdate(
       curPost._id,
       {
-        commentIds: newId,
+        $pull: { comments: { _id: commentId } },
       },
       {
-        new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
+        // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
         // false 일 때에는 업데이트 되기 전의 데이터를 반환합니다.
+        new: true,
       },
     ).exec();
     if (!post) {
       ctx.status = 404;
       return;
+    } else {
+      ctx.status = 204; // No Content (성공하기는 했지만 응답할 데이터는 없음)
     }
-  } catch (e) {
-    ctx.throw(500, e);
-  }
-
-  // 2. 댓글 컬렉션에서 해당 id 제거
-  try {
-    const _ = await Comment.findByIdAndRemove(commentId).exec();
-    ctx.status = 204; // No Content (성공하기는 했지만 응답할 데이터는 없음)
   } catch (e) {
     ctx.throw(500, e);
   }
@@ -115,43 +73,32 @@ export const cmDelete = async (ctx) => {
 
 /**
  * 댓글 좋아요
- * POST /api/post/comment/like/:commentId
+ * POST /api/post/:postId/comment/like/:commentId
  */
 export const cmLike = async (ctx) => {
-  const { commentId } = ctx.params;
+  const { postId, commentId } = ctx.params;
   const user = ctx.state.user;
 
-  // 1. 좋아요 증가
+  // TODO: 중복된 사용자에게는 좋아요, 아이디 저장 동작을 막는다
   try {
-    const _ = await Comment.findByIdAndUpdate(
-      commentId,
+    const result = await Post.findOneAndUpdate(
       {
-        $inc: { likes: 1 },
+        _id: postId,
+        'comments._id': commentId,
+      },
+      {
+        // 1. 좋아요 누른 유저 아이디 저장
+        $addToSet: { 'comments.$.likeMe': user._id },
+        // 2. 좋아요 수 증가
+        $inc: { 'comments.$.likes': 1 },
       },
       {
         new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
         // false 일 때에는 업데이트 되기 전의 데이터를 반환합니다.
       },
     ).exec();
+    ctx.body = result;
   } catch (e) {
     ctx.throw(500, e);
   }
-
-  // 2. 좋아요 누른 유저 아이디 저장
-  try {
-    const _ = await Comment.findByIdAndUpdate(
-      commentId,
-      {
-        $push: { likeMe: user._id },
-      },
-      {
-        new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
-        // false 일 때에는 업데이트 되기 전의 데이터를 반환합니다.
-      },
-    ).exec();
-  } catch (e) {
-    ctx.throw(500, e);
-  }
-
-  ctx.status = 204;
 };
--- backend/src/main.js
+++ backend/src/main.js
@@ -1,17 +1,17 @@
-require("dotenv").config();
-import Koa from "koa";
-import Router from "koa-router";
-import bodyParser from "koa-bodyparser";
-import mongoose from "mongoose";
+require('dotenv').config();
+import Koa from 'koa';
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import mongoose from 'mongoose';
 
-import api from "./api";
+import api from './api';
 
 const { PORT, MONGO_URI } = process.env;
 
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true })
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log('Connected to MongoDB');
   })
   .catch((e) => {
     console.error(e);
@@ -40,17 +40,17 @@ const router = new Router();
 app.use(bodyParser());
 
 // 라우터 설정
-router.use("/api", api.routes());
+router.use('/api', api.routes());
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
-const serve = require("koa-static");
+const serve = require('koa-static');
 // 이미지 파일 인식을 위해 static경로 설정
-app.use(serve("."));
+app.use(serve('.'));
 // console.log(__dirname + "./api/post/uploads/");
 
 const port = PORT || 4000;
 app.listen(port, () => {
-  console.log("Listening to port %d", port);
+  console.log('Listening to port %d', port);
 });

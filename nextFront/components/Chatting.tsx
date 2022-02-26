a77600e3214ec92a50dd648e95f5cad45cc7a250
jkim68888 <jkim68888@gmail.com>
2022-02-26 15:07:51 +0900

    [frontend] 1. 채팅방 UI 2. 대카테고리 정리

--- nextFront/components/Chatting.tsx
+++ nextFront/components/Chatting.tsx
@@ -1,7 +1,10 @@
 import { useState, useEffect, useRef } from 'react'
 import Image from 'next/image'
 import Box from 'assets/img/profile.svg'
+import CloseIcon from 'assets/img/close_icon.svg'
+import Icon from 'assets/img/chat_title_icon.svg'
 import styled from 'styled-components'
+import { categoryList } from 'static/constant'
 
 const Chatting = () => {
   const [vis_chat, setVisChat] = useState(false)
@@ -32,7 +35,21 @@ const Chatting = () => {
         <Image src={Box} width="50px" height="50px" alt="채팅 여는 이미지" />
       </OpenChattingBox>
       <ChattingBox ref={chat_box_ref} is_vis={vis_chat}>
-        ss
+        <ButtonWrap>
+          <Title>
+            DR.U 채팅방
+            <Image src={Icon} alt="아이콘" />
+          </Title>
+          <CloseButton onClick={() => setVisChat(false)}>
+            <Image src={CloseIcon} alt="닫기 버튼" />
+          </CloseButton>
+        </ButtonWrap>
+        {categoryList.map((item, idx) => (
+          <ChattingRoom key={idx}>
+            <h3>{item}</h3>
+            <EnterButton>입장</EnterButton>
+          </ChattingRoom>
+        ))}
       </ChattingBox>
     </>
   )
@@ -59,11 +76,11 @@ const OpenChattingBox = styled.button`
   bottom: 20px;
   right: 20px;
   transition: all 0.21s;
-  &:hover 
+  z-index: 10000;
+  &:hover {
     animation: arrowShaking 1.5s infinite;
   }
 `
-
 const ChattingBox = styled.div<{ is_vis?: boolean }>`
   position: fixed;
   bottom: 20px;
@@ -71,11 +88,51 @@ const ChattingBox = styled.div<{ is_vis?: boolean }>`
   width: 400px;
   height: 500px;
   background-color: #fff;
-  border: 1px solid #000;
+  border: 1px solid #1850a3;
+  border-radius: 5px;
   transition: all 0.21s;
+  z-index: 10001;
+  overflow-y: scroll;
 
   opacity: ${(props) => (props.is_vis ? 1 : 0)};
   transform: ${(props) =>
     props.is_vis ? 'translateY(0px)' : 'translateY(12px)'};
   visibility: ${(props) => (props.is_vis ? 'visible' : 'hidden')};
+`
+const Title = styled.h3`
+  margin-bottom: 0;
+  font-weight: 700;
+  color: #1850a3;
+
+  img {
+    width: 25px !important;
+  }
+`
+const ButtonWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 15px;
+  border-bottom: 1px solid #1850a3;
+`
+const CloseButton = styled.button`
+  transition: all 0.4s ease-in;
+  &:hover {
+    transform: rotate(360deg);
+  }
+`
+const ChattingRoom = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: calc(100% - 30px);
+  height: 70px;
+  margin: 15px;
+  border-bottom: 1px solid #c4c4c4;
+`
+const EnterButton = styled.button`
+  width: 80px;
+  height: calc(100% - 15px);
+  background-color: #1850a3;
+  border-radius: 5px;
+  color: #fff;
+  font-weight: 700;
 `

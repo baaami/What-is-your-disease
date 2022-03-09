2cf39913c33ef8885864f0e02aec599f71df4803
jeehoo0767 <jeehoo0767@naver.com>
2022-03-10 01:27:13 +0900

    frontend 디렉토리 제거

--- frontend/src/config.js
+++
@@ -1,8 +0,0 @@
-export const KAKAO_CLIENT_ID = '4eddab721534c723f493b770866d8b0a'
-
-export const NAVER_CLIENT_ID = 'sf2emA1qgadNTvuob2uo'
-export const NAVER_CLIENT_SECRET = '5iXgT4FOHH'
-
-export const GOOGLE_CLIENT_ID =
-  '490979667205-s2vp38r3dme8j58qsgdikc0ml6gumlp8.apps.googleusercontent.com'
-export const GOOGLE_CLIENT_SECRET = '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'

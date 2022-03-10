3750223de764a4473a6ac713e0ede1e3cb6f6965
SeongBeom lee <52270698+SeongBeom27@users.noreply.github.com>
2022-03-10 23:04:43 +0900

    [BackEnd] 푸쉬 알람 confirm 처리 response 개발
    [개발사항]
    1. 해당 사용자 confirm되지 않은 알람 개수 획득
    2. 해당 유저 푸쉬 리스트 송신 ( 날짜별 sort, confirm되지 않은 순서 우선적으로 )

--- backend/src/api/push/push.ctrl.js
+++ backend/src/api/push/push.ctrl.js
@@ -32,16 +32,61 @@ export const allRestoredPushData = async (ctx) => {
 export const confirm = async (ctx) => {
   const user = ctx.state.user;
   const { pushId } = ctx.query;
+  let data;
 
   try {
-    const pushData = await Push.findOneAndUpdate(
+    const _ = await Push.findOneAndUpdate(
       { _id: pushId },
       { confirm: true },
-      { new: true },
+      { new: false },
     );
 
     // 해당 user의 남은 푸쉬 알람 개수 전달
     // ctx.body = pushData;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+
+  try {
+    data = await Push.find({ receiver: user.info.nickname }).sort([
+      ['confirm', 1],
+      ['publishedDate', -1],
+    ]);
+
+    // 해당 user의 남은 푸쉬 알람 개수 전달
+    // ctx.body = pushData;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+
+  const notConfirmedDataCnt = await Push.countDocuments({
+    receiver: user.info.nickname,
+    confirm: false,
+  }).exec();
+
+  const resData = {
+    notConfirmedDataCnt: notConfirmedDataCnt,
+    data: data,
+  };
+
+  ctx.body = resData;
+};
+
+/**
+ * POST /api/push/readall
+ * 유저 정보 전달
+ */
+export const readall = async (ctx) => {
+  const user = ctx.state.user;
+
+  // 1. 자신의 followingId에 해당 Id 추가
+  try {
+    const _ = await Push.updateMany(
+      { receiver: user.info.nickname },
+      { $set: { confirm: true } },
+      { new: true },
+    );
+    ctx.status = 204;
   } catch (e) {
     ctx.throw(500, e);
   }

5acf76168d090fa617f055bd98f7e1c48b8335f2
SeongBeom lee <52270698+SeongBeom27@users.noreply.github.com>
2022-03-19 00:26:24 +0900

    [Backend] 인기 키워드 데이터 모델 생성

---
+++ backend/src/models/words.js
@@ -0,0 +1,14 @@
+import mongoose, { Schema } from 'mongoose';
+
+export const WordSchema = new Schema({
+  _id: mongoose.Types.ObjectId,
+  data: String,
+  publishedDate: {
+    // 보낸 시간
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Word = mongoose.model('Word', WordSchema);
+export default Word;
